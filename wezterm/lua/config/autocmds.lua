5bc50c4b54928d4c1a9f2d34874fd1c631339674
artumont <197291181+artumont@users.noreply.github.com>
2026-07-16 23:14:33 -0600

    feat: stuff

--- wezterm/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-
-local M = {}
-
-function M.apply(config)
-	return config
-end
-
-return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,8 +5,6 @@ local config = wezterm.config_builder()
 package.path = os.getenv("HOME") .. "/.config/wezterm/lua/?.lua;" .. package.path
 
 require("config.options").apply(config)
-require("config.keymaps").apply(config)
-require("plugins.smart-splits").apply(config)
 require("plugins.tabline").apply(config)
 
 -- tabline may override this; force native title bar last
