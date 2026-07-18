1e67f957dbb02d218db221d3df63ac89d9277619
artumont <197291181+artumont@users.noreply.github.com>
2026-07-18 11:56:10 -0600

    feat: agent stuff

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+-- Telescope
+
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+  },
+}
