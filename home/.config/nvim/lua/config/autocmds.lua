fe9063b60dbc86dbc888a6c9846030032b559e17
artumont <197291181+artumont@users.noreply.github.com>
2026-08-26 15:45:18 -0600

    feat: add gitignore templates

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -5,9 +5,48 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.schedule(function()
         vim.bo[args.buf].filetype = "neo-tree-git"
 
+        -- find an existing normal neo-tree window to copy props from
+        local ref_win
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].filetype == "neo-tree" then
+            ref_win = win
+            break
+          end
+        end
+
+        local opts_to_copy = {
+          "number",
+          "relativenumber",
+          "signcolumn",
+          "foldcolumn",
+          "wrap",
+          "list",
+          "spell",
+          "cursorline",
+          "cursorcolumn",
+          "winfixwidth",
+          "winfixheight",
+          "statuscolumn",
+        }
+
         for _, win in ipairs(vim.fn.win_findbuf(args.buf)) do
-          vim.wo[win].number = false
-          vim.wo[win].relativenumber = false
+          if ref_win then
+            for _, opt in ipairs(opts_to_copy) do
+              local ok, val = pcall(vim.api.nvim_get_option_value, opt, { win = ref_win })
+              if ok then vim.api.nvim_set_option_value(opt, val, { win = win }) end
+            end
+          else
+            -- fallback if no other neo-tree window is open yet
+            vim.wo[win].number = false
+            vim.wo[win].relativenumber = false
+          end
+        end
+
+        -- block opening files from this buffer
+        local noop = function() end
+        for _, key in ipairs { "<CR>", "o", "t", "s", "v" } do
+          vim.keymap.set("n", key, noop, { buffer = args.buf, nowait = true, silent = true })
         end
       end)
     end
---
+++ home/.config/nvim/lua/plugins/git/gitignore-templates.lua
@@ -0,0 +1,10 @@
+-- Generate .gitignore files from templates
+--
+
+return {
+  {
+    "artumont/gitignore-templates.nvim",
+    cmd = { "Gitignore", "GitignoreUpdate", "GitignoreClear" },
+    opts = {},
+  },
+}
