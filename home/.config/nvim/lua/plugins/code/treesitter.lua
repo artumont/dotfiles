3e3f3f995023dff7fb02b80f73c711c5dee22b24
artumont <197291181+artumont@users.noreply.github.com>
2026-08-26 00:06:24 -0600

    add treesitter config

---
+++ home/.config/nvim/lua/plugins/code/treesitter.lua
@@ -0,0 +1,26 @@
+-- Syntax highlighting and code parsing via tree-sitter
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "python",
+        "javascript",
+        "typescript",
+        "json",
+        "yaml",
+        "toml",
+        "markdown",
+        "bash",
+      },
+      auto_install = true,
+      indent = {
+        enabled = false,
+      },
+    },
+  },
+}
