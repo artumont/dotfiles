cedc1bded41bc9e9d968a74266d88a5288edde5f
artumont <197291181+artumont@users.noreply.github.com>
2026-08-27 11:35:05 -0600

    feat: remove a little padding from bufferline

--- home/.config/nvim/lua/plugins/core/bufferline.lua
+++ home/.config/nvim/lua/plugins/core/bufferline.lua
@@ -26,7 +26,7 @@ return {
           for _, pos in ipairs { "left", "right" } do
             local sb = layout[pos]
             if sb and #sb.wins > 0 then
-              local title = "        " .. string.rep(" ", sb.bounds.width - 8)
+              local title = "       " .. string.rep(" ", sb.bounds.width - 8)
               ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
               ret[pos .. "_size"] = sb.bounds.width
             end
